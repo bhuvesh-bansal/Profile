75733a9f306f2e5ef5bba3e71713bf5c2c469ff5
Bhuvesh bansal <96925288+bhuvesh-bansal@users.noreply.github.com>
2025-05-01 12:40:26 +0530

    fixed 404 error

--- Profile/vite.config.ts
+++ Profile/vite.config.ts
@@ -3,23 +3,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: true,
-    open: true
-  },
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined
       }
     }
   },
-  base: ''
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
 });
